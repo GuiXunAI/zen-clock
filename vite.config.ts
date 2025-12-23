dd13c1656688af2372b1219a3d274bb2ed7df750
LY <guixun.ai.01@gmail.com>
2025-12-23 18:01:16 +0800

    Update process.env definition and build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': '({})'
+    // 必须使用 JSON.stringify 以确保它被编译为 JS 对象字面量 {}
+    'process.env': JSON.stringify({}),
+    'global': 'window'
   },
   build: {
     outDir: 'dist',
-    target: 'esnext'
+    target: 'esnext',
+    emptyOutDir: true
   },
   server: {
     port: 3000
